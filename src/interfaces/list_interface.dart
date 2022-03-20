8c679b28991770709f61118c0c2b0dbe037f5ea7
Brabli <67018167+Brabli@users.noreply.github.com>
2022-03-20 18:34:18 +0000

    Remove comments

--- src/interfaces/list_interface.dart
+++ src/interfaces/list_interface.dart
@@ -1,16 +1,7 @@
 abstract class ListInterface {
-
   /// Adds a new row to the CSV file
   void add(String item);
 
   /// Removes a list item at the given ONE BASED index
   void remove(int index);
-
-  /*
-  Item CSV Fields
-  Item, Index, Time and Date Added, Tasks
-
-  History CSV Fields
-  Timestamp, Action, Item
-  */
 }
