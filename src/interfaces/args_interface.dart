55cd85d6094581fff084f86efc86ba0826f588a1
Brabli <67018167+Brabli@users.noreply.github.com>
2022-03-20 19:12:24 +0000

    Add args interface

---
+++ src/interfaces/args_interface.dart
@@ -0,0 +1,12 @@
+import '../enums/option.dart';
+
+abstract class ArgsInterface {
+  /// Returns the passed option
+  Option? get option;
+
+  /// Returns a list of any option specific arguments
+  List<dynamic> get optionArgs;
+
+  /// Returns an item to add if one was passed.
+  String? get item;
+}
