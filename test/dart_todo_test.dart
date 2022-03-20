ba75b44ffa752bbe3040b2e34aa0466755a38142
Brabli <67018167+Brabli@users.noreply.github.com>
2022-03-20 18:33:02 +0000

    Add tests

--- test/dart_todo_test.dart
+++ test/dart_todo_test.dart
@@ -1,15 +1,77 @@
 import 'dart:io';
+import 'package:csv/csv.dart';
 import 'package:test/test.dart';
 import '../src/todo_list.dart';
 
 void main() {
   group('TodoList', () {
-    test('TodoList creates a new file when initialised', () {
-      String path = 'test.csv';
-      expect(File(path).existsSync(), false);
-      TodoList(path);
-      expect(File(path).existsSync(), true);
+    String path = 'test.csv';
+
+    tearDown(() {
       File(path).deleteSync(recursive: true);
     });
+
+    test('TodoList creates a new file when initialised', () {
+      TodoList(path);
+      expect(File(path).existsSync(), true);
+    });
+
+    test('TodoList adds a new row to the list', () {
+      TodoList todoList = TodoList(path);
+      todoList.add('An example item');
+      expect(_countRows(todoList), 1);
+      todoList.add('An example item');
+      todoList.add('An example item');
+      expect(_countRows(todoList), 3);
+    });
+
+    group('remove()', () {
+      test('Removes an item at the given index', () {
+        TodoList todoList = TodoList(path);
+        todoList.add('Item 1');
+        todoList.add('Item 2');
+        todoList.remove(1);
+        expect(_countRows(todoList), 1);
+        expect(_getRows(todoList).first[0], 'Item 2');
+      });
+
+      test('Removes the last item if 0 is passed as the index', () {
+        TodoList todoList = TodoList(path);
+        todoList.add('Item 1');
+        todoList.add('Item 2');
+        todoList.remove(0);
+        expect(_countRows(todoList), 1);
+        expect(_getRows(todoList).first[0], 'Item 1');
+      });
+
+      test('Throws an exception if index is below zero', () {
+        TodoList todoList = TodoList(path);
+        expect(() => todoList.remove(-1), throwsException);
+      });
+
+      test('Throws an error if index is out of range', () {
+        TodoList todoList = TodoList(path);
+        expect(() => todoList.remove(1), throwsException);
+        expect(() => todoList.remove(0), throwsException);
+      });
+    });
   });
+}
+
+/// Returns the number of rows (excluding keys) in a CSV file
+int _countRows(TodoList todoList) {
+  int rows = CsvToListConverter().convert(todoList.toString()).length - 1;
+  if (rows < 0) {
+    throw Exception('List keys have been removed!');
+  }
+
+  return rows;
+}
+
+/// Returns a list of all the data rows in a CSV file
+List<List<dynamic>> _getRows(TodoList todoList) {
+  var rows = CsvToListConverter().convert(todoList.toString());
+  rows.removeAt(0);
+
+  return rows;
 }
