e8b9425b9f62410351459889d168e894cf1c6c79
Brabli <67018167+Brabli@users.noreply.github.com>
2022-04-02 19:42:27 +0100

    Fix captitalize extension

--- lib/args.dart
+++ lib/args.dart
@@ -98,15 +98,15 @@ class Args extends ArgsInterface {
   /// Returns the item or null if not provided
   String? _getItem(List<String> args) {
     if (_option == null) {
-      String item = args.join(' ').trim();
+      String item = args.join(' ').trim().capitalize();
+
       return item.isEmpty ? null : item;
     }
 
     args.removeFirst();
     if (args.isEmpty) return null;
-    args[0] = args[0].capitalize();
-    String item = args.join(' ').trim();
-    print(item);
+    String item = args.join(' ').trim().capitalize();
+
     return item;
   }
 }
@@ -123,6 +123,8 @@ extension RemoveFirst on List {
 
 extension StringExtension on String {
   String capitalize() {
-    return "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
+    return isEmpty
+        ? ''
+        : "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
   }
 }
