033691c03a8b47823cb35d90b3f132cdf8a2d7c6
Brabli <67018167+Brabli@users.noreply.github.com>
2022-04-09 21:59:55 +0100

    Split parser from args class

--- lib/args.dart
+++ lib/args.dart
@@ -2,17 +2,11 @@ import 'enum/option.dart';
 import 'interface/args_interface.dart';
 
 class Args extends ArgsInterface {
-  Option? _option;
-  List? _optionArgs;
-  String? _item;
+  final Option? _option;
+  final List? _optionArgs;
+  final String? _item;
 
-  Args(List<String> args) {
-    if (args.isNotEmpty) {
-      _option = _getOption(args[0]);
-      _optionArgs = _getOptionArgs(args[0]);
-      _item = _getItem(args);
-    }
-  }
+  Args(this._option, this._optionArgs, this._item);
 
   @override
   Option? get option {
@@ -27,113 +21,5 @@ class Args extends ArgsInterface {
   @override
   String? get item {
     return _item;
-  }
-
-  /// Returns the option provided as the first argument or null
-  Option? _getOption(String firstArg) {
-    if (firstArg == '-' || firstArg == '--') {
-      throw Exception('Expected an argument but none were found!');
-    }
-
-    if (firstArg.startsWith('--')) {
-      String option = firstArg.substring(2).split('=')[0];
-      return _getCorrectOption(option);
-    }
-
-    if (firstArg.startsWith('-')) {
-      String option = firstArg.substring(1, 2);
-      return _getCorrectOption(option);
-    }
-
-    return null;
-  }
-
-  /// Returns the appropriate option enum from a string
-  Option? _getCorrectOption(String option) {
-    switch (option) {
-      case 'remove':
-      case 'r':
-        return Option.remove;
-      default:
-        throw Exception('Invalid argument provided!');
-    }
-  }
-
-  // @TODO continue refactoring
-  /// Returns a List of option arguments or null
-  List<String>? _getOptionArgs(String firstArg) {
-    if (_option == null) {
-      return null;
-    }
-
-    if (firstArg.startsWith('--')) {
-      var splitBits = firstArg.split('=');
-      if (splitBits.length >= 2) {
-        splitBits.removeFirst();
-        String optionArgs = splitBits.reduce((val, ele) => val += ele);
-
-        if (optionArgs.isEmpty) {
-          throw Exception('No option arguments provided!');
-        }
-
-        List<String> args =
-            optionArgs.split(',').where((e) => e.isNotEmpty).toList();
-        if (args.isNotEmpty) {
-          return args;
-        }
-      }
-
-      return null;
-    }
-
-    if (firstArg.startsWith('-')) {
-      String stringArgs = firstArg.substring(2);
-      if (stringArgs.isNotEmpty) {
-        List<String> args =
-            stringArgs.split(',').where((e) => e.isNotEmpty).toList();
-        if (args.isNotEmpty) return args;
-      }
-    }
-
-    return null;
-  }
-
-  /// Returns the item or null if not provided
-  String? _getItem(List<String> args) {
-    if (_option == null) {
-      String item = args.join(' ').trim().capitalize();
-
-      return item.isEmpty ? null : item;
-    }
-
-    args.removeFirst();
-    if (args.isEmpty) return null;
-    String item = args.join(' ').trim().capitalize();
-
-    return item;
-  }
-}
-
-// @TODO Put this somewhere that makes sense and compare it to removeLast()
-extension RemoveFirst on List {
-  /// Removes and returns the first object in this list.
-  ///
-  /// The list must be growable and non-empty.
-  /// ```dart
-  /// final parts = <String>['head', 'shoulder', 'knees', 'toes'];
-  /// final retVal = parts.removeLast(); // head
-  /// print(parts); // [shoulder, knees, toes]
-  /// ```
-  dynamic removeFirst() {
-    return removeAt(0);
-  }
-}
-
-extension StringExtension on String {
-  /// Captializes the first letter of a string.
-  String capitalize() {
-    return isEmpty
-        ? ''
-        : "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
   }
 }
--- test/parser_test.dart
+++ test/parser_test.dart
@@ -1,34 +1,35 @@
 import 'package:test/test.dart';
 import 'package:dart_todo/args.dart';
 import 'package:dart_todo/enum/option.dart';
+import 'package:dart_todo/parser.dart';
 
 void main() {
-  group('Args.option', () {
+  group('Parser', () {
     test('Option is null if no option is passed', () {
-      Args args = Args(_createArgs('test'));
+      Args args = Parser.parse(_createArgs('test'));
       expect(args.option, null);
     });
 
     test('Throws exception if option is invalid', () {
       expect(() {
-        Args(['-']);
+        Parser.parse(['-']);
       }, throwsException);
       expect(() {
-        Args(['--']);
+        Parser.parse(['--']);
       }, throwsException);
       expect(() {
-        Args(['--zzz']);
+        Parser.parse(['--zzz']);
       }, throwsException);
     });
 
     test('Option is correct if a short flag is passed first', () {
-      Args args = Args(_createArgs('-r test'));
+      Args args = Parser.parse(_createArgs('-r test'));
       expect(args.option, Option.remove);
     });
 
     test('Option is correct if a long flag is passed first', () {
-      Args args1 = Args(_createArgs('--remove test'));
-      Args args2 = Args(_createArgs('--remove=1 test'));
+      Args args1 = Parser.parse(_createArgs('--remove test'));
+      Args args2 = Parser.parse(_createArgs('--remove=1 test'));
       expect(args1.option, Option.remove);
       expect(args2.option, Option.remove);
     });
@@ -36,8 +37,8 @@ void main() {
 
   group('Args.optionArgs', () {
     test('Option args is null if no arguments are provided', () {
-      Args args1 = Args(_createArgs('-r test'));
-      Args args2 = Args(_createArgs('--remove test'));
+      Args args1 = Parser.parse(_createArgs('-r test'));
+      Args args2 = Parser.parse(_createArgs('--remove test'));
       expect(args1.optionArgs, null);
       expect(args2.optionArgs, null);
     });
@@ -46,15 +47,15 @@ void main() {
         'Option args throws an exception if no arguments are provided after an equals sign',
         () {
       expect(() {
-        Args(['--remove=']);
+        Parser.parse(['--remove=']);
       }, throwsException);
     });
 
     test('Option args are returned in a list', () {
-      Args args1 = Args(_createArgs('--remove=1 test'));
-      Args args2 = Args(_createArgs('--remove=1,2,3 test'));
-      Args args3 = Args(_createArgs('-r1 test'));
-      Args args4 = Args(_createArgs('-r1,2,3 test'));
+      Args args1 = Parser.parse(_createArgs('--remove=1 test'));
+      Args args2 = Parser.parse(_createArgs('--remove=1,2,3 test'));
+      Args args3 = Parser.parse(_createArgs('-r1 test'));
+      Args args4 = Parser.parse(_createArgs('-r1,2,3 test'));
       expect(args1.optionArgs, ['1']);
       expect(args2.optionArgs, ['1', '2', '3']);
       expect(args3.optionArgs, ['1']);
@@ -62,15 +63,15 @@ void main() {
     });
 
     test('Option args ignore additional equals signs', () {
-      Args args = Args(_createArgs('--remove=1,=2,=3= test'));
+      Args args = Parser.parse(_createArgs('--remove=1,=2,=3= test'));
       expect(args.optionArgs, ['1', '2', '3']);
     });
 
     test('Option args does not include empty arguments', () {
-      Args args1 = Args(_createArgs('--remove=,,, test'));
-      Args args2 = Args(_createArgs('--remove=1,,,2 test'));
-      Args args3 = Args(_createArgs('-r,,, test'));
-      Args args4 = Args(_createArgs('-r1,,,2 test'));
+      Args args1 = Parser.parse(_createArgs('--remove=,,, test'));
+      Args args2 = Parser.parse(_createArgs('--remove=1,,,2 test'));
+      Args args3 = Parser.parse(_createArgs('-r,,, test'));
+      Args args4 = Parser.parse(_createArgs('-r1,,,2 test'));
       expect(args1.optionArgs, null);
       expect(args2.optionArgs, ['1', '2']);
       expect(args3.optionArgs, null);
@@ -82,24 +83,24 @@ void main() {
     String item = 'This is a test item';
 
     test('returns null if no item is found', () {
-      Args args1 = Args(_createArgs('-r1'));
-      Args args2 = Args(_createArgs('--remove'));
-      Args args3 = Args(_createArgs(''));
+      Args args1 = Parser.parse(_createArgs('-r1'));
+      Args args2 = Parser.parse(_createArgs('--remove'));
+      Args args3 = Parser.parse(_createArgs(''));
       expect(args1.item, null);
       expect(args2.item, null);
       expect(args3.item, null);
     });
 
     test('is returned when no options are passed', () {
-      Args args = Args(_createArgs(item));
+      Args args = Parser.parse(_createArgs(item));
       expect(args.item, item);
     });
 
     test('is returned when options are passed', () {
-      Args args1 = Args(_createArgs('-r $item'));
-      Args args2 = Args(_createArgs('-r1,2,3 $item'));
-      Args args3 = Args(_createArgs('--remove $item'));
-      Args args4 = Args(_createArgs('--remove=1 $item'));
+      Args args1 = Parser.parse(_createArgs('-r $item'));
+      Args args2 = Parser.parse(_createArgs('-r1,2,3 $item'));
+      Args args3 = Parser.parse(_createArgs('--remove $item'));
+      Args args4 = Parser.parse(_createArgs('--remove=1 $item'));
       expect(args1.item, item);
       expect(args2.item, item);
       expect(args3.item, item);
@@ -107,7 +108,7 @@ void main() {
     });
 
     test('capitalises the first letter of an item', () {
-      Args args = Args(_createArgs(item.toLowerCase()));
+      Args args = Parser.parse(_createArgs(item.toLowerCase()));
       expect(args.item, item);
     });
   });
