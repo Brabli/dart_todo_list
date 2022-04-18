adb6fd823ef6013c180379433ec36376ca2467c2
Brabli <67018167+Brabli@users.noreply.github.com>
2022-04-18 19:21:58 +0100

    Add method to interface

--- lib/interface/list_interface.dart
+++ lib/interface/list_interface.dart
@@ -4,4 +4,7 @@ abstract class ListInterface {
 
   /// Removes a list item at the given ONE BASED index
   void remove(int index);
+
+  /// Returns a List of all the items in the list
+  List<String> getItems();
 }
